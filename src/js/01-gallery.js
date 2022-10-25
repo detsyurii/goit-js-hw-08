06daadf83a128ac429203b9f225e9bb9d2cedf59
detsyurii <dets.yurii@gmail.com>
2022-10-25 12:31:05 +0300

    1 task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,41 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup();
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`
+    )
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+//////
+
+galleryRef.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
