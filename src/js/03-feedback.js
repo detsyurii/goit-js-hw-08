77966a2b4b0f99e7b881972b08f3290f845ec4c0
detsyurii <dets.yurii@gmail.com>
2022-10-30 15:45:51 +0200

    2 task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onFormInput, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+populateForm();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  localStorage.removeItem(STORAGE_KEY);
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  console.log({ email: email.value, message: message.value });
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(evt) {
+  const { name, value } = evt.target;
+  let savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    savedData = JSON.parse(savedData);
+  } else {
+    savedData = {};
+  }
+  savedData[name] = value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
+}
+
+function populateForm() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(savedData);
+  if (savedData) {
+    Object.entries(parsedData).forEach(([name, value]) => {
+      formRef.elements[name].value = value;
+    });
+  }
+}
